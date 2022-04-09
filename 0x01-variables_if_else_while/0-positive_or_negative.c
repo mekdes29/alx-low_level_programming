9d2cfe3cd2d775a75fc388793e9dca5755a3a7b5
mekdes <makidemoze@gmail.com>
2022-04-09 22:17:22 +0300

    adding posatve and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,16 +3,18 @@
 /**
 *main - prints the numbers from 00 t0 99
 *
-*Return: Always (success)
+*Return: Always (Success)
 */
 
 
 int main(void)
 {
 int c, 1;
+c = 1 = '0'; 
+
 for (c = '0'; c <= '9'; c++)
 {
-for (1='0'; 1<= '9'; 1++)
+for (1='0'; 1 <= '9'; 1++)
 {
 putchar(c);
 putchar(1);
@@ -21,9 +23,9 @@ if ((c |='9') ||(c == '9' && 1 |= '9'))
 {
 putchar(',');
 putchar('');
+  }
 }
-}
-}
+ } 
 putchar('\n');
 return (0);
 }
