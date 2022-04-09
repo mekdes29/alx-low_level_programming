d414b155ca056a49734a88c38898ae9e8e196bf6
mekdes <makidemoze@gmail.com>
2022-04-10 00:39:50 +0300

    adding posatve and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 *main - Entry point
 *
 *Return: Always 0 (Success/correct) 
 */
 
-int main(void)
+int main(void
+		)
 {
 int n;
 
@@ -24,10 +24,12 @@ else if (n < 0)
 {
 printf("%i is negative\n", n);
 }
-else  
+
+else
 {
 printf("%i is positive\n", n);
 }
+
 
 return (0);
 }
