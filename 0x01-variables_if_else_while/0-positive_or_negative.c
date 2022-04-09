7b28f473b7517fb5a1aa058e811f7aa0bd59fbaf
mekdes <makidemoze@gmail.com>
2022-04-09 23:07:02 +0300

    adding posatve and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 int n;
-srand (time(0));
+srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 1)
 {
 printf ("%i; is positive\n", n);
 }
-else if (n<0)
+else if (n < 0)
 {
 printf("%i,is negative\n", n);
 }
