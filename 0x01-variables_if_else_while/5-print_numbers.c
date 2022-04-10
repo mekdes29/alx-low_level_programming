4e299cb490cc334be3fffaac1bc8108c63283d90
mekdes <makidemoze@gmail.com>
2022-04-11 00:45:39 +0300

    adding 5 print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (success/correct)
+*/
+int main(void)
+
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%i, i);
+}
+
+printf('\n');
+
+return (0);
+}
