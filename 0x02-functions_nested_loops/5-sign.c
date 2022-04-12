4262b84227de41db1ed5e0492709530afcd95158
mekdes <makidemoze@gmail.com>
2022-04-13 00:11:50 +0300

    adding 5 sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,27 @@
+
 #include "main.h"
-#include <stdio.h>
 /**
- * _abs - value absolute
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
  *
- *@n: The int to print
+ *@n: the int to print
  * Return: Always 0.
  */
-
-int _abs(int n)
+int print_sign(int n)
 {
-
-if (n < 0)
+if (n > 0)
 {
-return (n * (-1));
+_putchar ('+');
+return (1);
 }
 
 else if (n == 0)
 {
+_putchar ('0');
 return (0);
 }
 
 else
 {
-return (n);
-}
-
+_putchar ('-');
+return (-1);
 }
