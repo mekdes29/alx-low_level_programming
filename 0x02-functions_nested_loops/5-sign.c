7564f01f0858536b922fc190a8bc5e8e46b65e46
mekdes <makidemoze@gmail.com>
2022-04-13 00:14:12 +0300

    adding 5 sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,24 @@
-
 #include "main.h"
 /**
- * print_sign - return 0 letter not lowercase, 1 letter lowercase
- *
- *@n: the int to print
- * Return: Always 0.
+ * print_sign - function that prints the sign of a number..
+ * @n: The input number to check
+ * Return: int.
  */
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar ('+');
-return (1);
-}
-
-else if (n == 0)
-{
-_putchar ('0');
-return (0);
-}
-
-else
-{
-_putchar ('-');
-return (-1);
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
