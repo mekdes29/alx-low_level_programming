c3955201997cc8e5ade9870944744a8abccca356
mekdes <makidemoze@gmail.com>
2022-04-18 21:46:42 +0300

    adding reset to 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,22 @@
-#include "holberton.h"
-
+#ifndef HOLBERTON_FUNTIONS
+#define HOLBERTON_FUNTIONS
 /**
- * reset_to_98 - check the code for Holberton School students.
- * @n: is a pointer int type
- * Return: Always 0.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-void reset_to_98(int *n)
-{
-*n = 98;
-}
+int _putchar(char c);  /* An example function declaration */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+#endif
